a7a6c9884c408cc417a71281d93a9e651fed0a7c
luojingwei <jonnylaw89@163.com>
2024-08-27 12:31:41 +0800

    添加h5文件处理部分注释

--- dataset_maker/make_h5dataset_for_pretrain.py
+++ dataset_maker/make_h5dataset_for_pretrain.py
@@ -4,6 +4,8 @@ from shock.utils import preprocessing_cnt
 
 savePath = Path('path/to/your/save/path')
 rawDataPath = Path('path/to/your/raw/data/path')
+
+# 使用 glob 方法从 rawDataPath 指定的目录中查找所有扩展名为 .cnt 的文件，并将其路径生成器对象赋值给 group。glob('*.cnt') 返回一个迭代器，包含符合条件的所有文件的路径。你可以通过迭代 group 来处理每个 .cnt 文件。
 group = rawDataPath.glob('*.cnt')
 
 # preprocessing parameters
@@ -19,7 +21,9 @@ for cntFile in group:
     print(f'processing {cntFile.name}')
     eegData, chOrder = preprocessing_cnt(cntFile, l_freq, h_freq, rsfreq)
     chOrder = [s.upper() for s in chOrder]
-    eegData = eegData[:, :-10*rsfreq]
+    eegData = eegData[:, :-10*rsfreq] # 每个session取10s的数据
+
+    # cntFile.stem 是 pathlib.Path 对象的一个属性，它返回路径中最后一个组件的“纯粹文件名”，即去掉扩展名后的文件名。
     grp = dataset.addGroup(grpName=cntFile.stem)
     dset = dataset.addDataset(grp, 'eeg', eegData, chunks)
 
--- dataset_maker/shock/utils/h5.py
+++ dataset_maker/shock/utils/h5.py
@@ -5,11 +5,14 @@ from pathlib import Path
 class h5Dataset:
     def __init__(self, path:Path, name:str) -> None:
         self.__name = name
+
+        # 'a': 这是文件访问模式。在 h5py.File 中，'a' 模式表示 "append"，即：如果文件存在，则打开它进行读写操作。如果文件不存在，则创建一个新的文件。
         self.__f = h5py.File(path / f'{name}.hdf5', 'a')
     
     def addGroup(self, grpName:str):
         return self.__f.create_group(grpName)
     
+    # 如果数据 data 是一个 N 维数组，那么 chunks 也应该是一个包含 N 个元素的元组，每个元素对应于数据集每一维的块大小。
     def addDataset(self, grp:h5py.Group, dsName:str, arr:np.array, chunks:tuple):
         return grp.create_dataset(dsName, data=arr, chunks=chunks)
     
